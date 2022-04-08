0f90f9cff1060d14689013b8fe169f0aee906325
Daniel Granaß <student2683@web.de>
2022-04-08 14:50:19 +0200

    Initial commit

---
+++ src/codeknacker/RunCodeKnacker.java
@@ -0,0 +1,28 @@
+package codeknacker;
+
+public class RunCodeKnacker {
+    public static void main(String[]args){
+        System.out.println("////Regeln:");
+        System.out.println("////Zulässige Zahlen sind: 0<X<100");
+        System.out.println("////Dezimalzahlen sind verboten");
+        System.out.println("////2 Punkte für die Eingabe einer richtigen Zahl");
+        System.out.println("////Kein Punkt für die Eingabe einer falschen Zahl");
+        System.out.println("////Bei falscher Eingabe erhählt der Spieler die Nachricht, ob die Zahl zu groß/klein war");
+        System.out.println("////Nach jeder Eingabe (egal ob richtige/ falsche Zahl) kommt der andere Spieler dran");
+        System.out.println("////Jeder Spieler hat 5 Versuche, um die jeweils richtige Zahl zu erraten");
+        System.out.println("////Am Ende eines Spiels wird der gesuchte Zahlencode ausgegeben.");
+        System.out.println("////Gewonnen hat derjenige, der die meisten Punkte am Ende einer Spielrunde gesammelt hat");
+        System.out.println("-------------------------------------------------------------------------------------------");
+        System.out.println("");
+        System.out.println("                              Willkommen beim Java-Spiel");
+        System.out.println("                              ------ CodeKnacker ------");
+        System.out.println("");
+        System.out.println("Vor Jahren hat sich Alice einen Safe zugelegt, um ihre Wertsachen in Sicherheit zu wissen.");
+        System.out.println("Leider kann sie diesen nicht mehr öffnen, weil sie ihren Code vergessen hat-(");
+        System.out.println("Damit der Code nicht in falsche Hände gerät, hat sie sich diese Zahlenkombination auch nicht notiert.");
+        System.out.println("");
+        System.out.println("Helft ihr bei der Suche nach dem Code!");
+        System.out.println("Wenn 2 Spieler feststehen...");
+        System.out.println("Das Spiel beginnt...");
+    }
+}
---
+++ test/codeknackertest/CodeKnackerTest.java
@@ -0,0 +1,4 @@
+package codeknackertest;
+
+public class CodeKnackerTest {
+}
